c477208f468c3285c701da9bbb8cdb6551b53758
Whitomtit <whitomtit@gmail.com>
2024-07-24 10:55:05 +0300

    fixed crlf issue with git

--- .devcontainer/fetch-git.sh
+++ .devcontainer/fetch-git.sh
@@ -1,3 +1,5 @@
+git config --global core.eol lf
+git config --global core.autocrlf input
 git init hw_files
 cd hw_files
 git remote add -f origin https://github.com/OpenUniversity/PL-236319-Spr-2024.git
